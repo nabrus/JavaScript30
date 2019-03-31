bf2a3032533c75a1c5579a798120c2e090945c23
towelie <34988577+nabrus@users.noreply.github.com>
2019-03-31 16:33:22 -0400

    start exercise, custom play bnt working

---
+++ 11-custom-video-player/scripts.js
@@ -0,0 +1,17 @@
+/* Get Elements */
+const player = document.querySelector(".viewer");
+const playBnts = document.querySelectorAll(".player__button");
+
+
+/* Build out functions */
+function bntToggle(e) {
+  console.log(e);
+  if (e.target.className === "player__button toggle") {
+    player.play();
+  } else {
+    player.pause();
+  }
+}
+
+/* Hook up the event listeners */
+playBnts.forEach(bnt => bnt.addEventListener("click", bntToggle));
