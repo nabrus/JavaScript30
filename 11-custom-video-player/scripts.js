a7016e6c314e7813a69a673027bb6b604ff915e1
towelie <34988577+nabrus@users.noreply.github.com>
2019-03-31 23:16:50 -0400

    togglePlay complete

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -25,5 +25,15 @@ const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
 
 /* Build out functions */
+function togglePlay() {
+  // If video is paused play it or pause it
+  // `.paused` property is used because there is no play property
+  return video.paused ? video.play() : video.pause();
+  // Wes's solution
+  // const method = video.paused ? 'play' : 'pause';
+  // video[method]();
+}
 
 /* Hook up the event listeners */
+video.addEventListener("click", togglePlay); // plays vid when viewer is clicked
+toggle.addEventListener("click", togglePlay); // plays vid when play bnt clicked
