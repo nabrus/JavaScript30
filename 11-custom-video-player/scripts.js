319d7e19ac1b1de748a2a18082cc9ace8f4f8c2e
towelie <34988577+nabrus@users.noreply.github.com>
2019-03-31 22:57:43 -0400

    start Wes's solution, elements selected

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -1,15 +1,29 @@
-/* Get Elements */
-const player = document.querySelector(".viewer");
-const buttons = document.querySelectorAll(".player__button");
+// /* Get Elements */
+// const player = document.querySelector(".viewer");
+// const buttons = document.querySelectorAll(".player__button");
 
+
+// /* Build out functions */
+// function bntToggle(e) {
+//   console.log(e);
+//   if (e.target.className === "player__button toggle") {
+//     player.play();
+//   }
+// }
+
+// /* Hook up the event listeners */
+// buttons.forEach(bnt => bnt.addEventListener("click", bntToggle));
+
+// Wes's solution
+/* Get Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
 /* Build out functions */
-function bntToggle(e) {
-  console.log(e);
-  if (e.target.className === "player__button toggle") {
-    player.play();
-  }
-}
 
 /* Hook up the event listeners */
-buttons.forEach(bnt => bnt.addEventListener("click", bntToggle));
