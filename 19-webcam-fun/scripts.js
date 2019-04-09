9ed5dbbe44022c16aaf1081579842370d4ed8903
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-09 01:06:06 -0400

    create link in takePhoto function

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -33,4 +33,21 @@ function paintToCanvas() {
   }, 16);
 }
 
+function takePhoto() {
+  // Play Audio
+  snap.currentTime = 0;
+  snap.play();
+
+  // Take the data out of the canvas...
+  const data = canvas.toDataURL("image/jpeg"); 
+  // Stores base64 data: binary-to-text encoding(binary data in a ASCII string).
+  const link = document.createElement("a"); // Create an anchor <a> link
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.textContent = "Download Image";
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
