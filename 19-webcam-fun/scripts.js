9eedb84077753abcc8a1545891450a5ca8e5aa61
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-09 09:14:39 -0400

    add comments to base64 encoding area

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -39,12 +39,18 @@ function takePhoto() {
   snap.play();
 
   // Take the data out of the canvas...
-  const data = canvas.toDataURL("image/jpeg"); 
+  const data = canvas.toDataURL("image/jpeg");
+  console.log(data);
   // Stores base64 data: binary-to-text encoding(binary data in a ASCII string).
+  // So, we're storing the image encoded as a text string that can be decoded
+  // into an image.
   const link = document.createElement("a"); // Create an anchor <a> link
   link.href = data;
-  link.setAttribute("download", "handsome");
+  link.setAttribute("download", "handsome"); // downloads to users comp when clicked
   link.textContent = "Download Image";
+  // console.log(link);
+  // <a href="data:image/jpeg;base64,/9j...PRo38H//Z" download="handsome">Download Image</a>
+  // strip.insertBefore(link, strip.firstChild);
   strip.insertBefore(link, strip.firstChild);
 }
 
