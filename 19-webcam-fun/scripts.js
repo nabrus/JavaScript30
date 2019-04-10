e44cb01db66bad5581099d15df68a5804f5b92c7
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-09 22:30:13 -0400

    complete exercise

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -1,8 +1,8 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
 
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -33,6 +33,18 @@ function paintToCanvas() {
   // by calling `clearInterval()` on it.
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // Take the pixels out of canvas
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // Mess with them
+    // pixels = redEffect(pixels);
+
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+
+    pixels = greenScreen(pixels);
+
+    // Put them back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -50,12 +62,58 @@ function takePhoto() {
   const link = document.createElement("a"); // Create an anchor <a> link
   link.href = data;
   link.setAttribute("download", "handsome"); // downloads to users comp when clicked
-  link.textContent = "Download Image";
+  link.innerHTML = `<img src="${data}" alt="selfie" />`;
   // console.log(link);
   // <a href="data:image/jpeg;base64,/9j...PRo38H//Z" download="handsome">Download Image</a>
   // strip.insertBefore(link, strip.firstChild);
   strip.insertBefore(link, strip.firstChild);
 }
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // green
+    pixels.data[i + 2] = pixels.data[i + 0] * 0.5; // blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // red
+    pixels.data[i + 200] = pixels.data[i + 1]; // green
+    pixels.data[i - 250] = pixels.data[i + 2]; // blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach(input => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    let red = pixels.data[i + 0];
+    let green = pixels.data[i + 1];
+    let blue = pixels.data[i + 2];
+    let alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
 
 getVideo();
 
