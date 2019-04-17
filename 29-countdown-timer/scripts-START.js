6770b013fd616dffe69e9a5814444fb3c55ff475
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-17 09:19:52 -0400

    update comments on timer fn

--- 29-countdown-timer/scripts-START.js
+++ 29-countdown-timer/scripts-START.js
@@ -4,12 +4,12 @@ function timer(seconds) {
   // When the timer started
   const now = Date.now(); // current timestamp in milliseconds
   const then = now + seconds * 1000; // secs * 1000 to get ms
+
   // Display amount of time left
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     // divide to get secs, round off the decimal
-    if (secondsLeft <= 0) {
-      // Condition to stop running it.
+    if (secondsLeft <= 0) { // Condition to stop running it.
       clearInterval(countdown);
     }
     // Display countdown
