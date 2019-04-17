ac2a038c6009fd2f62c4a887f32424b556bd0b34
towelie <34988577+nabrus@users.noreply.github.com>
2019-04-17 09:18:12 -0400

    start exercise, timer function

---
+++ 29-countdown-timer/scripts-START.js
@@ -0,0 +1,18 @@
+let countdown; // Global var, window
+
+function timer(seconds) {
+  // When the timer started
+  const now = Date.now(); // current timestamp in milliseconds
+  const then = now + seconds * 1000; // secs * 1000 to get ms
+  // Display amount of time left
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // divide to get secs, round off the decimal
+    if (secondsLeft <= 0) {
+      // Condition to stop running it.
+      clearInterval(countdown);
+    }
+    // Display countdown
+    console.log(secondsLeft);
+  }, 1000); // every second
+}
